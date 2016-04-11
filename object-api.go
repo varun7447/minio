cbef9115ff77f9dedd65533feb207e96fe9072db
Krishna Srinivas <krishna.srinivas@gmail.com>
2016-04-11 13:59:18 +0530

    refactor: add multipart code to the object layer.

--- object-api.go
+++ object-api.go
@@ -19,15 +19,21 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"errors"
 	"fmt"
 	"io"
+	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/minio/minio/pkg/mimedb"
 	"github.com/minio/minio/pkg/probe"
 	"github.com/minio/minio/pkg/safe"
+	"github.com/skyrings/skyring-common/tools/uuid"
+)
+
+const (
+	minioMetaVolume = ".minio"
 )
 
 type objectAPI struct {
@@ -315,15 +321,44 @@ func (o objectAPI) ListObjects(bucket, prefix, marker, delimiter string, maxKeys
 	return result, nil
 }
 
-func (o objectAPI) ListMultipartUploads(bucket, objectPrefix, keyMarker, uploadIDMarker, delimiter string, maxUploads int) (ListMultipartsInfo, *probe.Error) {
+func (o objectAPI) ListMultipartUploads(bucket, prefix, marker, uploadIDMarker, delimiter string, maxUploads int) (ListMultipartsInfo, *probe.Error) {
+	result := ListMultipartsInfo{}
 	// Verify if bucket is valid.
 	if !IsValidBucketName(bucket) {
 		return ListMultipartsInfo{}, probe.NewError(BucketNameInvalid{Bucket: bucket})
 	}
-	if !IsValidObjectPrefix(objectPrefix) {
-		return ListMultipartsInfo{}, probe.NewError(ObjectNameInvalid{Bucket: bucket, Object: objectPrefix})
+	if !IsValidObjectPrefix(prefix) {
+		return ListMultipartsInfo{}, probe.NewError(ObjectNameInvalid{Bucket: bucket, Object: prefix})
 	}
-	return ListMultipartsInfo{}, probe.NewError(errors.New("Not implemented"))
+	// Verify if delimiter is anything other than '/', which we do not support.
+	if delimiter != "" && delimiter != "/" {
+		return ListMultipartsInfo{}, probe.NewError(fmt.Errorf("delimiter '%s' is not supported. Only '/' is supported", delimiter))
+	}
+	// Verify if marker has prefix.
+	if marker != "" {
+		if !strings.HasPrefix(marker, prefix) {
+			return ListMultipartsInfo{}, probe.NewError(fmt.Errorf("Invalid combination of marker '%s' and prefix '%s'", marker, prefix))
+		}
+	}
+	recursive := true
+	if delimiter == "/" {
+		recursive = false
+	}
+	fileInfos, _, e := o.storage.ListFiles(minioMetaVolume, filepath.Join(bucket, prefix)+string(os.PathSeparator), "", recursive, maxUploads)
+	if e != nil {
+		return result, probe.NewError(e)
+	}
+	for _, fileInfo := range fileInfos {
+		fileName := filepath.Base(fileInfo.Name)
+		if !strings.Contains(fileName, ".") {
+			result.Uploads = append(result.Uploads, uploadMetadata{
+				Object:    prefix,
+				UploadID:  fileName,
+				Initiated: fileInfo.ModTime,
+			})
+		}
+	}
+	return result, nil
 }
 
 func (o objectAPI) NewMultipartUpload(bucket, object string) (string, *probe.Error) {
@@ -334,7 +369,23 @@ func (o objectAPI) NewMultipartUpload(bucket, object string) (string, *probe.Err
 	if !IsValidObjectName(object) {
 		return "", probe.NewError(ObjectNameInvalid{Bucket: bucket, Object: object})
 	}
-	return "", probe.NewError(errors.New("Not implemented"))
+	// FIXME: move this to init code
+	o.storage.MakeVol(minioMetaVolume)
+	uuid, e := uuid.New()
+	if e != nil {
+		return "", probe.NewError(e)
+	}
+	uploadID := uuid.String()
+	path := filepath.Join(bucket, object, uploadID)
+	w, e := o.storage.CreateFile(minioMetaVolume, path)
+	if e != nil {
+		return "", probe.NewError(e)
+	}
+	e = w.Close()
+	if e != nil {
+		return "", probe.NewError(e)
+	}
+	return uploadID, nil
 }
 
 func (o objectAPI) PutObjectPart(bucket, object, uploadID string, partID int, size int64, data io.Reader, md5Hex string) (string, *probe.Error) {
@@ -345,7 +396,55 @@ func (o objectAPI) PutObjectPart(bucket, object, uploadID string, partID int, si
 	if !IsValidObjectName(object) {
 		return "", probe.NewError(ObjectNameInvalid{Bucket: bucket, Object: object})
 	}
-	return "", probe.NewError(errors.New("Not implemented"))
+	partSuffix := fmt.Sprintf("%s.%d.%s", uploadID, partID, md5Hex)
+
+	fileWriter, e := o.storage.CreateFile(minioMetaVolume, filepath.Join(bucket, object, partSuffix))
+	if e != nil {
+		if e == errVolumeNotFound {
+			return "", probe.NewError(BucketNotFound{
+				Bucket: bucket,
+			})
+		} else if e == errIsNotRegular {
+			return "", probe.NewError(ObjectExistsAsPrefix{
+				Bucket: bucket,
+				Prefix: object,
+			})
+		}
+		return "", probe.NewError(e)
+	}
+
+	// Initialize md5 writer.
+	md5Writer := md5.New()
+
+	// Instantiate a new multi writer.
+	multiWriter := io.MultiWriter(md5Writer, fileWriter)
+
+	// Instantiate checksum hashers and create a multiwriter.
+	if size > 0 {
+		if _, e = io.CopyN(multiWriter, data, size); e != nil {
+			fileWriter.(*safe.File).CloseAndRemove()
+			return "", probe.NewError(e)
+		}
+	} else {
+		if _, e = io.Copy(multiWriter, data); e != nil {
+			fileWriter.(*safe.File).CloseAndRemove()
+			return "", probe.NewError(e)
+		}
+	}
+
+	newMD5Hex := hex.EncodeToString(md5Writer.Sum(nil))
+	if md5Hex != "" {
+		if newMD5Hex != md5Hex {
+			fmt.Println(md5Hex, newMD5Hex)
+			fileWriter.(*safe.File).CloseAndRemove()
+			return "", probe.NewError(BadDigest{md5Hex, newMD5Hex})
+		}
+	}
+	e = fileWriter.Close()
+	if e != nil {
+		return "", probe.NewError(e)
+	}
+	return newMD5Hex, nil
 }
 
 func (o objectAPI) ListObjectParts(bucket, object, uploadID string, partNumberMarker, maxParts int) (ListPartsInfo, *probe.Error) {
@@ -356,7 +455,47 @@ func (o objectAPI) ListObjectParts(bucket, object, uploadID string, partNumberMa
 	if !IsValidObjectName(object) {
 		return ListPartsInfo{}, probe.NewError(ObjectNameInvalid{Bucket: bucket, Object: object})
 	}
-	return ListPartsInfo{}, probe.NewError(errors.New("Not implemented"))
+	result := ListPartsInfo{}
+	marker := ""
+	nextPartNumberMarker := 0
+	if partNumberMarker > 0 {
+		fileInfos, _, e := o.storage.ListFiles(minioMetaVolume, filepath.Join(bucket, object, uploadID)+"."+strconv.Itoa(partNumberMarker)+".", "", false, 1)
+		if e != nil {
+			return result, probe.NewError(e)
+		}
+		if len(fileInfos) == 0 {
+			return result, probe.NewError(InvalidPart{})
+		}
+		marker = fileInfos[0].Name
+	}
+	fileInfos, eof, e := o.storage.ListFiles(minioMetaVolume, filepath.Join(bucket, object, uploadID)+".", marker, false, maxParts)
+	if e != nil {
+		return result, probe.NewError(InvalidPart{})
+	}
+	for _, fileInfo := range fileInfos {
+		fileName := filepath.Base(fileInfo.Name)
+		splitResult := strings.Split(fileName, ".")
+		partNum, e := strconv.Atoi(splitResult[1])
+		if e != nil {
+			return result, probe.NewError(e)
+		}
+		md5sum := splitResult[2]
+		result.Parts = append(result.Parts, partInfo{
+			PartNumber:   partNum,
+			LastModified: fileInfo.ModTime,
+			ETag:         md5sum,
+			Size:         fileInfo.Size,
+		})
+		nextPartNumberMarker = partNum
+	}
+	result.Bucket = bucket
+	result.Object = object
+	result.UploadID = uploadID
+	result.PartNumberMarker = partNumberMarker
+	result.NextPartNumberMarker = nextPartNumberMarker
+	result.MaxParts = maxParts
+	result.IsTruncated = !eof
+	return result, nil
 }
 
 func (o objectAPI) CompleteMultipartUpload(bucket string, object string, uploadID string, parts []completePart) (ObjectInfo, *probe.Error) {
@@ -367,7 +506,66 @@ func (o objectAPI) CompleteMultipartUpload(bucket string, object string, uploadI
 	if !IsValidObjectName(object) {
 		return ObjectInfo{}, probe.NewError(ObjectNameInvalid{Bucket: bucket, Object: object})
 	}
-	return ObjectInfo{}, probe.NewError(errors.New("Not implemented"))
+	fileWriter, e := o.storage.CreateFile(bucket, object)
+	if e != nil {
+		return ObjectInfo{}, nil
+	}
+	for _, part := range parts {
+		fileReader, e := o.storage.ReadFile(minioMetaVolume, filepath.Join(bucket, object, fmt.Sprintf("%s.%d.%s", uploadID, part.PartNumber, part.ETag)), 0)
+		if e != nil {
+			fmt.Println(e, filepath.Join(bucket, object, fmt.Sprintf("%s.%s.%s", uploadID, part.PartNumber, part.ETag)))
+			return ObjectInfo{}, probe.NewError(e)
+		}
+		_, e = io.Copy(fileWriter, fileReader)
+		if e != nil {
+			return ObjectInfo{}, probe.NewError(e)
+		}
+		e = fileReader.Close()
+		if e != nil {
+			return ObjectInfo{}, probe.NewError(e)
+		}
+	}
+	e = fileWriter.Close()
+	if e != nil {
+		return ObjectInfo{}, probe.NewError(e)
+	}
+	fi, e := o.storage.StatFile(bucket, object)
+	if e != nil {
+		return ObjectInfo{}, probe.NewError(e)
+	}
+	o.removeMultipartUpload(bucket, object, uploadID)
+	return ObjectInfo{
+		Bucket:  bucket,
+		Name:    object,
+		ModTime: fi.ModTime,
+		Size:    fi.Size,
+		IsDir:   false,
+	}, nil
+}
+
+func (o objectAPI) removeMultipartUpload(bucket, object, uploadID string) *probe.Error {
+	// Verify if bucket is valid.
+	if !IsValidBucketName(bucket) {
+		return probe.NewError(BucketNameInvalid{Bucket: bucket})
+	}
+	if !IsValidObjectName(object) {
+		return probe.NewError(ObjectNameInvalid{Bucket: bucket, Object: object})
+	}
+	marker := ""
+	for {
+		fileInfos, eof, e := o.storage.ListFiles(minioMetaVolume, filepath.Join(bucket, object, uploadID), marker, false, 1000)
+		if e != nil {
+			return probe.NewError(ObjectNotFound{Bucket: bucket, Object: object})
+		}
+		for _, fileInfo := range fileInfos {
+			o.storage.DeleteFile(minioMetaVolume, fileInfo.Name)
+			marker = fileInfo.Name
+		}
+		if eof {
+			break
+		}
+	}
+	return nil
 }
 
 func (o objectAPI) AbortMultipartUpload(bucket, object, uploadID string) *probe.Error {
@@ -378,5 +576,9 @@ func (o objectAPI) AbortMultipartUpload(bucket, object, uploadID string) *probe.
 	if !IsValidObjectName(object) {
 		return probe.NewError(ObjectNameInvalid{Bucket: bucket, Object: object})
 	}
-	return probe.NewError(errors.New("Not implemented"))
+	e := o.removeMultipartUpload(bucket, object, uploadID)
+	if e != nil {
+		return e.Trace()
+	}
+	return nil
 }
