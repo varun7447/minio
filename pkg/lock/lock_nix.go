feab480ac13be471f5e72e5afb9ff01cd416c8e4
Krishna Srinivas <krishna.srinivas@gmail.com>
2017-08-02 01:54:17 -0700

    Disable Fcntl locking on linux

--- pkg/lock/lock_nix.go
+++ pkg/lock/lock_nix.go
@@ -19,7 +19,6 @@
 package lock
 
 import (
-	"fmt"
 	"os"
 	"syscall"
 )
@@ -27,33 +26,33 @@ import (
 // Internal function implements support for both
 // blocking and non blocking lock type.
 func lockedOpenFile(path string, flag int, perm os.FileMode, lockType int) (*LockedFile, error) {
-	switch flag {
-	case syscall.O_RDONLY:
-		lockType |= syscall.LOCK_SH
-	case syscall.O_WRONLY:
-		fallthrough
-	case syscall.O_RDWR:
-		fallthrough
-	case syscall.O_WRONLY | syscall.O_CREAT:
-		fallthrough
-	case syscall.O_RDWR | syscall.O_CREAT:
-		lockType |= syscall.LOCK_EX
-	default:
-		return nil, fmt.Errorf("Unsupported flag (%d)", flag)
-	}
+	// switch flag {
+	// case syscall.O_RDONLY:
+	// 	lockType |= syscall.LOCK_SH
+	// case syscall.O_WRONLY:
+	// 	fallthrough
+	// case syscall.O_RDWR:
+	// 	fallthrough
+	// case syscall.O_WRONLY | syscall.O_CREAT:
+	// 	fallthrough
+	// case syscall.O_RDWR | syscall.O_CREAT:
+	// 	lockType |= syscall.LOCK_EX
+	// default:
+	// 	return nil, fmt.Errorf("Unsupported flag (%d)", flag)
+	// }
 
 	f, err := os.OpenFile(path, flag|syscall.O_SYNC, perm)
 	if err != nil {
 		return nil, err
 	}
 
-	if err = syscall.Flock(int(f.Fd()), lockType); err != nil {
-		f.Close()
-		if err == syscall.EWOULDBLOCK {
-			err = ErrAlreadyLocked
-		}
-		return nil, err
-	}
+	// if err = syscall.Flock(int(f.Fd()), lockType); err != nil {
+	// 	f.Close()
+	// 	if err == syscall.EWOULDBLOCK {
+	// 		err = ErrAlreadyLocked
+	// 	}
+	// 	return nil, err
+	// }
 
 	st, err := os.Stat(path)
 	if err != nil {
